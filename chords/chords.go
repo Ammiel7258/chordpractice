98548a6d79ddb70296971d73f3c3afb2d89e3064
Ammiel7258 <ammiel.maynard@gmail.com>
2025-04-09 00:07:42 -0400

    initial commit

---
+++ chords/chords.go
@@ -0,0 +1,13 @@
+package chords
+
+var UnusedChords []string
+var UsedChords = []string{
+	"A",
+	"C",
+	"D",
+	"E",
+	"G",
+	"Am",
+	"Dm",
+	"Em",
+}
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"chordpractice/chords"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+var unusedChords []string
+var usedChords []string
+var bpm int
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	w.SetContent(container.NewVBox(
+		widget.NewLabel("Hello from WSL!"),
+	))
+
+	w.Resize(fyne.NewSize(300, 200))
+	w.ShowAndRun()
+}
+
+func draw() {
+
+}
+
+func menu() {
+	fmt.Println("What bpm will you be strumming at?")
+	fmt.Scan(&bpm)
+
+	fmt.Printf("Displaying chords to play at %d bmp! Press CTRL + C to stop!\n", bpm)
+
+	unusedChords = chords.UnusedChords
+	usedChords = chords.UsedChords
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	ticker := time.NewTicker(time.Minute / time.Duration(bpm))
+
+	for {
+		<-ticker.C
+
+		fmt.Printf("%s\n", getRandomChord())
+	}
+}
+
+func getRandomChord() string {
+	if len(unusedChords) < 1 {
+		unusedChords = append(unusedChords, usedChords[0:]...)
+		usedChords = usedChords[:0]
+	}
+
+	index := rand.Intn(len(unusedChords))
+	chord := unusedChords[index]
+
+	usedChords = append(usedChords, chord)
+	unusedChords = append(unusedChords[:index], unusedChords[index+1:]...)
+
+	return chord
+}
+
+/*
+	"bytes"
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/go-mp3"
+
+	fileBytes, err := os.ReadFile("metronome.mp3")
+	if err != nil {
+		panic("reading metronome.mp3 failed: " + err.Error())
+	}
+
+	fileBytesReader := bytes.NewReader(fileBytes)
+
+	decodedMp3, err := mp3.NewDecoder(fileBytesReader)
+  if err != nil {
+    panic("mp3.NewDecoder failed: " + err.Error())
+  }
+
+  op := &oto.NewContextOptions{
+    SampleRate: 44100,
+    ChannelCount: 2,
+    Format: oto.FormatSignedInt16LE,
+  }
+
+  otoCtx, readyChan, err := oto.NewContext(op)
+  if err != nil {
+    panic("oto.NewContext failed: " + err.Error())
+  }
+  <-readyChan
+
+  player := otoCtx.NewPlayer(decodedMp3)
+
+  player.Play()
+
+  for player.IsPlaying() {
+    time.Sleep(time.Millisecond)
+  }
+
+  err = player.Close()
+  if err != nil {
+      panic("player.Close failed: " + err.Error())
+  }
+*/
